3c20f25f45310f22ae1ceeec964ab3968acea905
Petri Sipinen <petri.sipinen@centria.fi>
2021-06-07 16:42:42 +0300

    7.6.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+
 
 const App = () => {
-  console.log(props)
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -14,9 +16,9 @@ const App = () => {
   return (
     <div>
       <h2>give feedback</h2>
-      <Button handleClick = {() => setGood(good + 1)} text = "good"/>
-      <Button handleClick ={() => setNeutral(neutral + 1)} text = "neutral"/>
-      <Button handleClick={() => setBad(bad + 1)} text = "bad"/>
+      <Button handleClick = {() => setGood(good + 1)} text = "Good"/>
+      <Button handleClick ={() => setNeutral(neutral + 1)} text = "Neutral"/>
+      <Button handleClick={() => setBad(bad + 1)} text = "Bad"/>
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} total= {total} average = {average} positive={positive}/>
     </div>
@@ -24,8 +26,8 @@ const App = () => {
 }
 
 
-
 const Stat = (props) => {
+  console.log(props)
   return (
      <div>
        {props.name} {props.value} {props.percentage}
@@ -34,8 +36,9 @@ const Stat = (props) => {
 }
 
 const Statistics = (props) => {
+  console.log(props)
   if ( props.total === 0) {
-    return <p>No feedback given</p>;
+    return <p>Waiting for feedback...</p>;
   }
   return (
      <div>
@@ -50,6 +53,7 @@ const Statistics = (props) => {
 }
 
 const Button = (props) => {
+  console.log(props)
   return (
      <button onClick = {props.handleClick}>
        {props.text}
