750a18e4fce88b2a2cba2702f1e662299763de9a
Petri Sipinen <petri.sipinen@centria.fi>
2021-06-07 17:56:55 +0300

    7.6.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,64 +1,68 @@
 import React, { useState } from 'react'
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-
-
-
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const total = good + neutral + bad
-  const average = (good - bad) / total
-  const positive = (good / total)* 100
-
-
+const Label = ({selected,votes}) => {
   return (
-    <div>
-      <h2>Give feedback</h2>
-      <Button handleClick = {() => setGood(good + 1)} text = "Good"/>
-      <Button handleClick ={() => setNeutral(neutral + 1)} text = "Neutral"/>
-      <Button handleClick={() => setBad(bad + 1)} text = "Bad"/>
-      <h2>Statistics</h2>
-      <Statistics good={good} neutral={neutral} bad={bad} total= {total} average = {average} positive={positive}/>
-    </div>
+      <>
+      <div>{anecdotes[selected]}</div>
+      <div>Has {votes[selected]} votes</div>
+      </>
   )
 }
 
 
-const Stat = (props) => {
-  console.log(props)
+const statistics = (votes, dataArray) => {
+  let i = votes.indexOf(Math.max(...votes))
   return (
-     <div>
-       {props.name} {props.value} {props.percentage}
-     </div> 
+      <div>
+      <h1>Anecdote with most votes</h1>
+      <div>{dataArray[i]}</div>
+      <div>Has {votes[i]} votes</div>
+      </div>
   )
 }
 
-const Statistics = (props) => {
-  console.log(props)
-  if ( props.total === 0) {
-    return <p>Waiting for feedback...</p>;
-  }
-  return (
-     <div>
-      <Stat name = "Good" value = {props.good}/>
-      <Stat name = "Neutral" value = {props.neutral} />
-      <Stat name = "Bad" value = {props.bad}/>
-      <Stat name = "Total" value = {props.total} />
-      <Stat name = "Average" value = {props.average} />
-      <Stat name = "Positive" value = {props.positive} percentage = "%" />
-     </div> 
-  )
+const App = (props) => {
+
+const dataLenght = props.anecdotes
+
+const [selected, setSelected] = useState(0)
+const [votes, setVote] = useState(Array(dataLenght).fill(0))
+
+
+const setRandom = (anecdotes) => {
+  setSelected(Math.floor(Math.random() * anecdotes))
 }
 
-const Button = (props) => {
-  console.log(props)
-  return (
-     <button onClick = {props.handleClick}>
-       {props.text}
-     </button>
-  )
+const giveVote = (selected) => {
+  const copy = [...votes]
+  copy[selected] += 1
+  setVote(copy)
 }
+
+return (
+  <div>
+    <Label selected={selected} votes={votes}/>
+    <p>
+    <Button handleClick={() => giveVote(selected)} text="Vote" />
+    <Button handleClick={() => setRandom(anecdotes)} text="Next anecdote" />
+    </p>
+    <div>{statistics(votes, props.anecdotes)}</div>
+  </div>
+)
+}
+
+const anecdotes = [
+'If it hurts, do it more often',
+'Adding manpower to a late software project makes it later!',
+'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+'Premature optimization is the root of all evil.',
+'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 export default App
