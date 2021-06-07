ee20ebc909bbce33437953f718131f9fab16c5ca
Petri Sipinen <petri.sipinen@centria.fi>
2021-06-07 17:06:40 +0300

    7.6.

--- part1/UniCafe - Second task/src/App.js
+++ part1/UniCafe - Second task/src/App.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react'
 
-
-
-
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -15,12 +12,12 @@ const App = () => {
 
   return (
     <div>
-      <h2>give feedback</h2>
+      <h2>Give feedback</h2>
       <Button handleClick = {() => setGood(good + 1)} text = "Good"/>
       <Button handleClick ={() => setNeutral(neutral + 1)} text = "Neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text = "Bad"/>
-      <h2>statistics</h2>
-      <Statistics Good={Good} Neutral={Neutral} Bad={Bad} Total= {Total} Average = {Average} Positive={Positive}/>
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} total= {total} average = {average} positive={positive}/>
     </div>
   )
 }
