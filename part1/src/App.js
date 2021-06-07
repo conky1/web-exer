0a6a0b4e8678243e2e969616ea107295d556a29e
Petri Sipinen <petri.sipinen@centria.fi>
2021-06-07 16:16:10 +0300

    7.6.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,73 +1,59 @@
 import React from 'react'
 
-const Header = (props) => {
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = (good / total)* 100
+
+
   return (
-        <div>
-          <h1>
-            {props.course.name}
-          </h1>
-        </div>
-    )
-  }
-  
-  const Content = (props) => {
-    return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick = {() => setGood(good + 1)} text = "good"/>
+      <Button handleClick ={() => setNeutral(neutral + 1)} text = "neutral"/>
+      <Button handleClick={() => setBad(bad + 1)} text = "bad"/>
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} total= {total} average = {average} positive={positive}/>
+    </div>
+  )
+}
+
+
+
+const Stat = (props) => {
+  return (
      <div>
-         <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-         <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-         <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
-        
-     </div>
-   )
+       {props.name} {props.value} {props.percentage}
+     </div> 
+  )
+}
+
+const Statistics = (props) => {
+  if ( props.total === 0) {
+    return <p>No feedback given</p>;
   }
-  
-  const Part = (props) => {
-    return (
-        <div>
-          <p>
-             {props.part} {props.exercises}
-          </p>
-        </div>
-      )
-  }
-  
-  const Total = (props) => {
   return (
-        <div>
-          <p>
-          Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} 
-          </p>
-        </div>
-   )
-  }
-  
-  
-  const App = () => {
-    const course = {
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7
-        },
-        {
-          name: 'State of a component',
-          exercises: 14
-        }
-      ]
-    }
-  
-     return (
-      <div>
-        <Header course={course}/>
-        <Content parts={course.parts}/>
-        <Total parts={course.parts}/>
-      </div>
-     )
-  }
+     <div>
+      <Stat name = "good" value = {props.good}/>
+      <Stat name = "neutral" value = {props.neutral} />
+      <Stat name = "bad" value = {props.bad}/>
+      <Stat name = "total" value = {props.total} />
+      <Stat name = "average" value = {props.average} />
+      <Stat name = "positive" value = {props.positive} percentage = "%" />
+     </div> 
+  )
+}
+
+const Button = (props) => {
+  return (
+     <button onClick = {props.handleClick}>
+       {props.text}
+     </button>
+  )
+}
 
 export default App
