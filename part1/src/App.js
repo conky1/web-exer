4b88cc7495da79252ba8bc80aeea17eb32a608af
Petri Sipinen <petri.sipinen@centria.fi>
2021-06-08 16:02:40 +0300

    8.6.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,64 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import Course from './components' 
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = (good / total)* 100
 
-  const App = () => {
-    const courses = [
-      {
-        name: 'Half Stack application development',
-        id: 1,
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
-          },
-          {
-            name: 'Redux',
-            exercises: 11,
-            id: 4
-          }
-        ]
-      }, 
-      {
-        name: 'Node.js',
-        id: 2,
-        parts: [
-          {
-            name: 'Routing',
-            exercises: 3,
-            id: 1
-          },
-          {
-            name: 'Middlewares',
-            exercises: 7,
-            id: 2
-          }
-        ]
-      }
-    ]
 
   return (
     <div>
-      {courses.map(course=>
-       <Course key={course.id} course={course}/>
-      )}
-      </div>
+      <h2>Give feedback</h2>
+      <Button handleClick = {() => setGood(good + 1)} text = "Good"/>
+      <Button handleClick ={() => setNeutral(neutral + 1)} text = "Neutral"/>
+      <Button handleClick={() => setBad(bad + 1)} text = "Bad"/>
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} total= {total} average = {average} positive={positive}/>
+    </div>
   )
 }
 
 
+const Stat = (props) => {
+  console.log(props)
+  return (
+     <div>
+       {props.name} {props.value} {props.percentage}
+     </div> 
+  )
+}
 
-export default App  
+const Statistics = (props) => {
+  console.log(props)
+  if ( props.total === 0) {
+    return <p>Waiting for feedback...</p>;
+  }
+  return (
+     <div>
+      <Stat name = "Good" value = {props.good}/>
+      <Stat name = "Neutral" value = {props.neutral} />
+      <Stat name = "Bad" value = {props.bad}/>
+      <Stat name = "Total" value = {props.total} />
+      <Stat name = "Average" value = {props.average} />
+      <Stat name = "Positive" value = {props.positive} percentage = "%" />
+     </div> 
+  )
+}
+
+const Button = (props) => {
+  console.log(props)
+  return (
+     <button onClick = {props.handleClick}>
+       {props.text}
+     </button>
+  )
+}
+
+export default App
